08d3688e86011459a572461cc5b61fa9cfb0f020
Dhruv Wagh <waghdhruv@gmail.com>
2023-08-24 16:28:40 +0300

    update4

--- resource_type_queue.py
+++ resource_type_queue.py
@@ -11,16 +11,24 @@ VALUE_BLOCK_SIZE = 4 * 1024  # 4KB
 # Function to continuously read user input and append values to the queue
 def input_reader():
     while True:
-        logical_block_address = input("Enter a logical block address (64 characters): ")
-        logical_block_address = logical_block_address[:64].ljust(64, '0')  # Limit to 64 characters and pad with zeros
-        value = input("Enter a value (up to 4KB): ")
-        value_size = len(value.encode())  # Size in bytes
-        if value_size > VALUE_BLOCK_SIZE:
-            print("Value size cannot exceed 4KB.")
-            continue
+        try:
+            key = int(input("Enter a key (integer): "))
+            if key < 0 or key >= 2**64:
+                print("Key must be a non-negative integer less than 2^64.")
+                continue
+            
+            value = input("Enter a value (up to 4KB): ")
+            value_size = len(value.encode())  # Size in bytes
+            if value_size > VALUE_BLOCK_SIZE:
+                print("Value size cannot exceed 4KB.")
+                continue
+            
+            binary_key = format(key, '064b')  # Convert key to 64-bit binary string
+            data_queue.put((binary_key, value))
+            time.sleep(1)  # Just to avoid overwhelming the console
         
-        data_queue.put((logical_block_address, value))
-        time.sleep(1)  # Just to avoid overwhelming the console
+        except ValueError:
+            print("Invalid input. Key must be a non-negative integer.")
 
 # Start the input_reader thread
 input_thread = threading.Thread(target=input_reader, daemon=True)
@@ -29,8 +37,8 @@ input_thread.start()
 # Main loop to process data from the queue
 while True:
     try:
-        logical_block_address, value = data_queue.get(timeout=1)  # Wait for up to 1 second
-        print(f"Received: Logical Block Address: {logical_block_address}, Value: {value}")
+        binary_key, value = data_queue.get(timeout=1)  # Wait for up to 1 second
+        print(f"Received: Binary Key: {binary_key}, Value: {value}")
         data_queue.task_done()  # Mark the task as done
     except queue.Empty:
         pass  # No data in the queue, continue processing or do other tasks
