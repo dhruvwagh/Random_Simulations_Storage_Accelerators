4002034eddd423af2d3f07ac2c6f3f9f42b8a1ba
Dhruv Wagh <waghdhruv@gmail.com>
2023-08-27 17:13:46 +0300

    up 27/08

--- rtq2.py
+++ rtq2.py
@@ -1,6 +1,7 @@
 import queue
 import threading
 import time
+import random
 
 # Create thread-safe queues for original key-value pairs and buckets
 original_data_queue = queue.Queue()
@@ -13,26 +14,13 @@ VALUE_BLOCK_SIZE = 4 * 1024  # 4KB
 def generate_hash_key(key):
     return hash(key) % (2**64)
 
-# Function to continuously read user input and append values to the original queue
-def input_reader():
+# Function to continuously generate random key-value pairs and append them to the original queue
+def input_generator():
     while True:
-        try:
-            key = int(input("Enter a key (integer): "))
-            if key < 0 or key >= 2**64:
-                print("Key must be a non-negative integer less than 2^64.")
-                continue
-            
-            value = input("Enter a value (up to 4KB): ")
-            value_size = len(value.encode())  # Size in bytes
-            if value_size > VALUE_BLOCK_SIZE:
-                print("Value size cannot exceed 4KB.")
-                continue
-            
-            original_data_queue.put((key, value))
-            time.sleep(1)  # Just to avoid overwhelming the console
-        
-        except ValueError:
-            print("Invalid input. Key must be a non-negative integer.")
+        key = random.randint(0, 2**64 - 1)
+        value = f"Random Value for Key {key}"
+        original_data_queue.put((key, value))
+        time.sleep(1)  # Just to avoid overwhelming the console
 
 # Function to take key-value pairs from the original queue,
 # extract the hash key, and put them in the appropriate bucket queue
@@ -41,14 +29,16 @@ def bucket_sort():
         try:
             key, value = original_data_queue.get(timeout=1)  # Wait for up to 1 second
             hash_key = generate_hash_key(key)
-            msbs = (hash_key >> 61) & 0b111  # Extract 3 MSBs
+            msbs = (hash_key >> 32) & 0b111  # Extract 3 MSBs
+            print(msbs)
+            print(f"Received (Bucket {msbs}): Hash Key: {hash_key}, Value: {value}")
             buckets[msbs].put((hash_key, value))
             original_data_queue.task_done()  # Mark the task as done
         except queue.Empty:
             pass  # No data in the original queue, continue processing or do other tasks
 
-# Start the input_reader and bucket_sort threads
-input_thread = threading.Thread(target=input_reader, daemon=True)
+# Start the input_generator and bucket_sort threads
+input_thread = threading.Thread(target=input_generator, daemon=True)
 input_thread.start()
 
 bucket_sort_thread = threading.Thread(target=bucket_sort, daemon=True)
@@ -56,10 +46,14 @@ bucket_sort_thread.start()
 
 # Main loop to process data from the buckets
 while True:
+    all_empty = True
     for i in range(8):
         try:
             hash_key, value = buckets[i].get_nowait()
-            print(f"Received (Bucket {i}): Hash Key: {hash_key}, Value: {value}")
+            print(f"Processed (Bucket {i}): Hash Key: {hash_key}, Value: {value}")
             buckets[i].task_done()
+            all_empty = False
         except queue.Empty:
             pass  # No data in the bucket queue, continue processing or do other tasks
+    if all_empty:
+        time.sleep(1)  # Avoid busy-waiting when all bucket queues are empty
