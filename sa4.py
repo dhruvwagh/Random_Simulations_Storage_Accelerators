58d560159e5e39a9090754ee9edc44c572606eaf
Dhruv Wagh <waghdhruv@gmail.com>
2023-09-02 23:55:15 +0300

    up2/9

--- sa4.py
+++ sa4.py
@@ -34,6 +34,7 @@ def input_func(arrival_buffer):
         value = input("Enter the value: ")
         kv_pair = key_value_pair(key, value)
         arrival_buffer.put(kv_pair)
+        print(arrival_buffer.qsize())
     time.sleep(1)
 
 def output(kv_pair):
@@ -41,37 +42,43 @@ def output(kv_pair):
 
 def bucket_sort(msbs, kv_pair, hashed_key):
     i = msbs
+    print("Bucket sorting into: ", i)
     hkv_pair = (hashed_key, kv_pair.value)
     hashed_buffers[i].put(hkv_pair)
+    print("buffer size:" ,hashed_buffers[i].qsize())
 
 def kv_process(arrival_buffer):
     print("kv_process thread started.")
     while True:
         if arrival_buffer.empty():
             print("kv_process: Waiting for input...")
-            time.sleep(1)
+            time.sleep(5)
         else:
             kv_p = arrival_buffer.get()
             print("kv_process: Processing", kv_p.key)
             hashed_key = hash_function(kv_p.key)
             msbs = extract_msbs(hashed_key, msb_number)
             bucket_sort(msbs, kv_p, hashed_key)
+            print("finished processing hash key:",hashed_key)
+            print("arrival buffer after processsing: ",arrival_buffer.qsize())
 
 def switch(hashed_buffers):
     print("switch thread started.")
     while True:
-        for i in range(msb_number):
+        for i in range(number_of_ssds):
+            print("Switching for bucket", i)
+            print("size of hashed bucket:",hashed_buffers[i].qsize())
             if not hashed_buffers[i].empty():
                 kv_pair = hashed_buffers[i].get()
                 print("switch: Outputting", kv_pair)
                 output(kv_pair)
+        time.sleep(5)
 
 arrival_buffer = queue.Queue()
 
-input_thread = threading.Thread(target=input_func, args=(arrival_buffer,))
+input_thread = threading.Thread(target=input_func, args=(arrival_buffer,),daemon=True)
 sort_thread = threading.Thread(target=kv_process, args=(arrival_buffer,))
-spit_thread = threading.Thread(target=switch, args=(hashed_buffers,))
-
+spit_thread = threading.Thread(target=switch, args=(hashed_buffers,),daemon=True)
 input_thread.start()
 sort_thread.start()
 spit_thread.start()
